c28b9f94491d5fb3cf02b1e23d7fcc310e404c7e
John D. Hume <duelin.markers@gmail.com>
2011-04-26 00:45:53 -0400

    Create Before and After for DateTimeOffset.

--- FluentTime.Tests/TimeSpanExtensionTest.cs
+++ FluentTime.Tests/TimeSpanExtensionTest.cs
@@ -20,5 +20,22 @@ namespace FluentTime.Tests
 			                new DateTime(2011, 4, 24, 10, 0, 0), 
 			                1.Day().After(new DateTime(2011, 4, 23, 10, 0, 0)));
 		}
+		
+		[Test]
+		public void Creates_a_DateTimeOffset_before_the_given_DateTimeOffset_maintaining_the_Offset()
+		{
+			var expected = DateTimeOffset.Parse("2011/04/26 12:17:00 AM -4:00");
+			var actual = 2.Hours().Before(DateTimeOffset.Parse("2011/04/26 2:17:00 AM -4:00"));
+			Assert.AreEqual(expected.DateTime, actual.DateTime);
+			Assert.AreEqual(expected.Offset, actual.Offset);
+		}
+		[Test]
+		public void Creates_a_DateTimeOffset_after_the_given_DateTimeOffset_maintaining_the_Offset()
+		{
+			var expected = DateTimeOffset.Parse("2011/04/26 12:17:00 AM -5:00");
+			var actual = 3.Hours().After(DateTimeOffset.Parse("2011/04/25 9:17:00 PM -5:00"));
+			Assert.AreEqual(expected.DateTime, actual.DateTime);
+			Assert.AreEqual(expected.Offset, actual.Offset);
+		}
 	}
 }
--- FluentTime/TimeSpanExtension.cs
+++ FluentTime/TimeSpanExtension.cs
@@ -3,14 +3,10 @@ namespace FluentTime
 {
 	public static class TimeSpanExtension
 	{
-		public static DateTime Before(this TimeSpan span, DateTime dateTime)
-		{
-			return dateTime - span;
-		}
+		public static DateTime       Before(this TimeSpan span, DateTime dateTime)       { return dateTime - span; }
+		public static DateTimeOffset Before(this TimeSpan span, DateTimeOffset dateTime) { return dateTime - span; }
 		
-		public static DateTime After(this TimeSpan span, DateTime dateTime)
-		{
-			return dateTime + span;
-		}
+		public static DateTime       After(this TimeSpan span, DateTime dateTime)       { return dateTime + span; }
+		public static DateTimeOffset After(this TimeSpan span, DateTimeOffset dateTime) { return dateTime + span; }
 	}
 }
