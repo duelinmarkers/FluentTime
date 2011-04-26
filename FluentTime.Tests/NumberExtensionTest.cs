f8ed1f10c4837ed4803379156f4b3e101c0aa543
John D. Hume <duelin.markers@gmail.com>
2011-04-26 02:06:05 -0400

    Add Week/Weeks methods.

--- FluentTime.Tests/NumberExtensionTest.cs
+++ FluentTime.Tests/NumberExtensionTest.cs
@@ -10,6 +10,7 @@ namespace FluentTime.Tests
 		[Test]
 		public void Creates_TimeSpans_readably_with_ints ()
 		{
+			Assert.That(2.Weeks(),        Is.EqualTo(TimeSpan.FromDays(14)));
 			Assert.That(2.Days(),         Is.EqualTo(TimeSpan.FromDays(2)));
 			Assert.That(2.Hours(),        Is.EqualTo(TimeSpan.FromHours(2)));
 			Assert.That(2.Minutes(),      Is.EqualTo(TimeSpan.FromMinutes(2)));
@@ -21,16 +22,24 @@ namespace FluentTime.Tests
 		[Test]
 		public void Creates_TimeSpans_readably_with_doubles ()
 		{
+			Assert.That(1.5.Weeks(), Is.EqualTo(new TimeSpan(10, 12, 0, 0)));
 			Assert.That(1.5.Days(), Is.EqualTo(new TimeSpan(1, 12, 0, 0)));
 			Assert.That(1.5.Hours(), Is.EqualTo(new TimeSpan(1, 30, 0)));
 			Assert.That(1.5.Minutes(), Is.EqualTo(new TimeSpan(0, 1, 30)));
 			Assert.That(1.5.Seconds(), Is.EqualTo(new TimeSpan(0, 0, 0, 1, 500)));
-			Assert.That(1.5.Milliseconds(), Is.EqualTo(new TimeSpan((long)(TimeSpan.TicksPerMillisecond * 1.5))));
+			Assert.That(1.5.Milliseconds(), Is.EqualTo(new TimeSpan((TimeSpan.TicksPerMillisecond * 3) / 2)));
+		}
+		
+		[Test]
+		public void Note_difference_with_TimeSpanDotFromMilliseconds_which_accepts_double_but_is_only_millisecond_precise ()
+		{
+			Assert.AreEqual(TimeSpan.FromMilliseconds(2), TimeSpan.FromMilliseconds(1.5));
 		}
 		
 		[Test]
 		public void Creates_TimeSpans_readably_with_singular_aliases ()
 		{
+			Assert.That(1.Week(),        Is.EqualTo(TimeSpan.FromDays(7)));
 			Assert.That(1.Day(),         Is.EqualTo(TimeSpan.FromDays(1)));
 			Assert.That(1.Hour(),        Is.EqualTo(TimeSpan.FromHours(1)));
 			Assert.That(1.Minute(),      Is.EqualTo(TimeSpan.FromMinutes(1)));
@@ -42,13 +51,13 @@ namespace FluentTime.Tests
 		[Test]
 		public void Stacks_TimeSpans_readably_in_plural_and_singular ()
 		{
-			Assert.That(2.Days(3.Hours(4.Minutes(15.Seconds(5.Milliseconds())))), 
-			            Is.EqualTo(new TimeSpan(2, 3, 4, 15, 5)));
+			Assert.That(2.Weeks(2.Days(3.Hours(4.Minutes(15.Seconds(5.Milliseconds()))))), 
+			            Is.EqualTo(new TimeSpan(16, 3, 4, 15, 5)));
 
 			Assert.That(2.Milliseconds(3.Ticks(4.Ticks())), Is.EqualTo(new TimeSpan((2 * TimeSpan.TicksPerMillisecond) + 7)));
 
-			Assert.That(1.Day(1.Hour(1.Minute(1.Second(1.Millisecond(1.Tick()))))), 
-			            Is.EqualTo(24.Hours(60.Minutes(60.Seconds(1001.Milliseconds()))) + 1.Tick()));
+			Assert.That(1.Week(1.Day(1.Hour(1.Minute(1.Second(1.Millisecond(1.Tick())))))), 
+			            Is.EqualTo(7.Days(24.Hours(60.Minutes(60.Seconds(1001.Milliseconds()))) + 1.Tick())));
 		}
 	}
 }
--- FluentTime/NumberExtension.cs
+++ FluentTime/NumberExtension.cs
@@ -3,13 +3,15 @@ namespace FluentTime
 {
 	public static class NumberExtension
 	{
+		public static TimeSpan Weeks(this double i) { return TimeSpan.FromDays(i * 7); }
 		public static TimeSpan Days(this double i) { return TimeSpan.FromDays(i); }
 		public static TimeSpan Hours(this double i) { return TimeSpan.FromHours(i); }
 		public static TimeSpan Minutes(this double i) { return TimeSpan.FromMinutes(i); }
 		public static TimeSpan Seconds(this double i) { return TimeSpan.FromSeconds(i); }
-		public static TimeSpan Milliseconds(this double i) { return TimeSpan.FromMilliseconds(i); }
+		public static TimeSpan Milliseconds(this double i) { return new TimeSpan((long)(TimeSpan.TicksPerMillisecond * i)); }
 		public static TimeSpan Ticks(this long i) { return TimeSpan.FromTicks(i); }
 
+		public static TimeSpan Week(this double i) { return Weeks(i); }
 		public static TimeSpan Day(this double i) { return Days(i); }
 		public static TimeSpan Hour(this double i) { return Hours(i); }
 		public static TimeSpan Minute(this double i) { return Minutes(i); }
@@ -17,6 +19,7 @@ namespace FluentTime
 		public static TimeSpan Millisecond(this double i) { return Milliseconds(i); }
 		public static TimeSpan Tick(this long i) { return Ticks(i); }
 
+		public static TimeSpan Weeks(this double i, TimeSpan otherTime) { return Weeks(i) + otherTime; }
 		public static TimeSpan Days(this double i, TimeSpan otherTime) { return Days(i) + otherTime; }
 		public static TimeSpan Hours(this double i, TimeSpan otherTime) { return Hours(i) + otherTime; }
 		public static TimeSpan Minutes(this double i, TimeSpan otherTime) { return Minutes(i) + otherTime; }
@@ -24,6 +27,7 @@ namespace FluentTime
 		public static TimeSpan Milliseconds(this double i, TimeSpan otherTime) { return Milliseconds(i) + otherTime; }
 		public static TimeSpan Ticks(this long i, TimeSpan otherTime) { return Ticks(i) + otherTime; }
 
+		public static TimeSpan Week(this double i, TimeSpan otherTime) { return Weeks(i, otherTime); }
 		public static TimeSpan Day(this double i, TimeSpan otherTime) { return Days(i, otherTime); }
 		public static TimeSpan Hour(this double i, TimeSpan otherTime) { return Hours(i, otherTime); }
 		public static TimeSpan Minute(this double i, TimeSpan otherTime) { return Minutes(i, otherTime); }
