7a6645f853f1fb8893ad6f39ae709529231bf6a5
John D. Hume <duelin.markers@gmail.com>
2011-04-26 01:05:35 -0400

    Stack TimeSpans with something like 2.Hours(10.Minutes()).

--- FluentTime.Tests/Int32ExtensionTest.cs
+++ FluentTime.Tests/Int32ExtensionTest.cs
@@ -19,7 +19,7 @@ namespace FluentTime.Tests
 		}
 		
 		[Test]
-		public void Supports_singular_aliases ()
+		public void Creates_TimeSpans_readably_with_singular_aliases ()
 		{
 			Assert.That(1.Day(), Is.EqualTo(TimeSpan.FromDays(1)));
 			Assert.That(1.Hour(), Is.EqualTo(TimeSpan.FromHours(1)));
@@ -27,6 +27,16 @@ namespace FluentTime.Tests
 			Assert.That(1.Second(), Is.EqualTo(TimeSpan.FromSeconds(1)));
 			Assert.That(1.Millisecond(), Is.EqualTo(TimeSpan.FromMilliseconds(1)));
 			Assert.That(1.Tick(), Is.EqualTo(TimeSpan.FromTicks(1)));
+		}
+		
+		[Test]
+		public void Stacks_TimeSpans_readably_in_plural_and_singular ()
+		{
+			Assert.That(2.Days(3.Hours(4.Minutes(15.Seconds()))), 
+			            Is.EqualTo(new TimeSpan(2, 3, 4, 15)));
+
+			Assert.That(1.Day(1.Hour(1.Minute(1.Second()))), 
+			            Is.EqualTo(24.Hours(60.Minutes(60.Seconds(1000.Milliseconds())))));
 		}
 	}
 }
--- FluentTime/Int32Extension.cs
+++ FluentTime/Int32Extension.cs
@@ -16,5 +16,19 @@ namespace FluentTime
 		public static TimeSpan Second(this int i) { return Seconds(i); }
 		public static TimeSpan Millisecond(this int i) { return Milliseconds(i); }
 		public static TimeSpan Tick(this int i) { return Ticks(i); }
+
+		public static TimeSpan Days(this int i, TimeSpan otherTime) { return Days(i) + otherTime; }
+		public static TimeSpan Hours(this int i, TimeSpan otherTime) { return Hours(i) + otherTime; }
+		public static TimeSpan Minutes(this int i, TimeSpan otherTime) { return Minutes(i) + otherTime; }
+		public static TimeSpan Seconds(this int i, TimeSpan otherTime) { return Seconds(i) + otherTime; }
+		public static TimeSpan Milliseconds(this int i, TimeSpan otherTime) { return Milliseconds(i) + otherTime; }
+		public static TimeSpan Ticks(this int i, TimeSpan otherTime) { return Ticks(i) + otherTime; }
+
+		public static TimeSpan Day(this int i, TimeSpan otherTime) { return Days(i, otherTime); }
+		public static TimeSpan Hour(this int i, TimeSpan otherTime) { return Hours(i, otherTime); }
+		public static TimeSpan Minute(this int i, TimeSpan otherTime) { return Minutes(i, otherTime); }
+		public static TimeSpan Second(this int i, TimeSpan otherTime) { return Seconds(i, otherTime); }
+		public static TimeSpan Millisecond(this int i, TimeSpan otherTime) { return Milliseconds(i, otherTime); }
+		public static TimeSpan Tick(this int i, TimeSpan otherTime) { return Ticks(i, otherTime); }
 	}
 }
