1d32b098afb3a59e5d52b4d4ff01314a6ec7af1f
John D. Hume <duelin.markers@gmail.com>
2011-04-26 01:35:22 -0400

    Change Int32Extension into NumberExtension and accept the same types as the underlying TimeSpan methods.

---
+++ FluentTime.Tests/NumberExtensionTest.cs
@@ -0,0 +1,54 @@
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace FluentTime.Tests
+{
+	[TestFixture]
+	public class NumberExtensionTest
+	{
+		[Test]
+		public void Creates_TimeSpans_readably_with_ints ()
+		{
+			Assert.That(2.Days(),         Is.EqualTo(TimeSpan.FromDays(2)));
+			Assert.That(2.Hours(),        Is.EqualTo(TimeSpan.FromHours(2)));
+			Assert.That(2.Minutes(),      Is.EqualTo(TimeSpan.FromMinutes(2)));
+			Assert.That(2.Seconds(),      Is.EqualTo(TimeSpan.FromSeconds(2)));
+			Assert.That(2.Milliseconds(), Is.EqualTo(TimeSpan.FromMilliseconds(2)));
+			Assert.That(2.Ticks(),        Is.EqualTo(TimeSpan.FromTicks(2)));
+		}
+		
+		[Test]
+		public void Creates_TimeSpans_readably_with_doubles ()
+		{
+			Assert.That(1.5.Days(), Is.EqualTo(new TimeSpan(1, 12, 0, 0)));
+			Assert.That(1.5.Hours(), Is.EqualTo(new TimeSpan(1, 30, 0)));
+			Assert.That(1.5.Minutes(), Is.EqualTo(new TimeSpan(0, 1, 30)));
+			Assert.That(1.5.Seconds(), Is.EqualTo(new TimeSpan(0, 0, 0, 1, 500)));
+			Assert.That(1.5.Milliseconds(), Is.EqualTo(new TimeSpan((long)(TimeSpan.TicksPerMillisecond * 1.5))));
+		}
+		
+		[Test]
+		public void Creates_TimeSpans_readably_with_singular_aliases ()
+		{
+			Assert.That(1.Day(),         Is.EqualTo(TimeSpan.FromDays(1)));
+			Assert.That(1.Hour(),        Is.EqualTo(TimeSpan.FromHours(1)));
+			Assert.That(1.Minute(),      Is.EqualTo(TimeSpan.FromMinutes(1)));
+			Assert.That(1.Second(),      Is.EqualTo(TimeSpan.FromSeconds(1)));
+			Assert.That(1.Millisecond(), Is.EqualTo(TimeSpan.FromMilliseconds(1)));
+			Assert.That(1.Tick(),        Is.EqualTo(TimeSpan.FromTicks(1)));
+		}
+		
+		[Test]
+		public void Stacks_TimeSpans_readably_in_plural_and_singular ()
+		{
+			Assert.That(2.Days(3.Hours(4.Minutes(15.Seconds(5.Milliseconds())))), 
+			            Is.EqualTo(new TimeSpan(2, 3, 4, 15, 5)));
+
+			Assert.That(2.Milliseconds(3.Ticks(4.Ticks())), Is.EqualTo(new TimeSpan((2 * TimeSpan.TicksPerMillisecond) + 7)));
+
+			Assert.That(1.Day(1.Hour(1.Minute(1.Second(1.Millisecond(1.Tick()))))), 
+			            Is.EqualTo(24.Hours(60.Minutes(60.Seconds(1001.Milliseconds()))) + 1.Tick()));
+		}
+	}
+}
---
+++ FluentTime/NumberExtension.cs
@@ -0,0 +1,34 @@
+using System;
+namespace FluentTime
+{
+	public static class NumberExtension
+	{
+		public static TimeSpan Days(this double i) { return TimeSpan.FromDays(i); }
+		public static TimeSpan Hours(this double i) { return TimeSpan.FromHours(i); }
+		public static TimeSpan Minutes(this double i) { return TimeSpan.FromMinutes(i); }
+		public static TimeSpan Seconds(this double i) { return TimeSpan.FromSeconds(i); }
+		public static TimeSpan Milliseconds(this double i) { return TimeSpan.FromMilliseconds(i); }
+		public static TimeSpan Ticks(this long i) { return TimeSpan.FromTicks(i); }
+
+		public static TimeSpan Day(this double i) { return Days(i); }
+		public static TimeSpan Hour(this double i) { return Hours(i); }
+		public static TimeSpan Minute(this double i) { return Minutes(i); }
+		public static TimeSpan Second(this double i) { return Seconds(i); }
+		public static TimeSpan Millisecond(this double i) { return Milliseconds(i); }
+		public static TimeSpan Tick(this long i) { return Ticks(i); }
+
+		public static TimeSpan Days(this double i, TimeSpan otherTime) { return Days(i) + otherTime; }
+		public static TimeSpan Hours(this double i, TimeSpan otherTime) { return Hours(i) + otherTime; }
+		public static TimeSpan Minutes(this double i, TimeSpan otherTime) { return Minutes(i) + otherTime; }
+		public static TimeSpan Seconds(this double i, TimeSpan otherTime) { return Seconds(i) + otherTime; }
+		public static TimeSpan Milliseconds(this double i, TimeSpan otherTime) { return Milliseconds(i) + otherTime; }
+		public static TimeSpan Ticks(this long i, TimeSpan otherTime) { return Ticks(i) + otherTime; }
+
+		public static TimeSpan Day(this double i, TimeSpan otherTime) { return Days(i, otherTime); }
+		public static TimeSpan Hour(this double i, TimeSpan otherTime) { return Hours(i, otherTime); }
+		public static TimeSpan Minute(this double i, TimeSpan otherTime) { return Minutes(i, otherTime); }
+		public static TimeSpan Second(this double i, TimeSpan otherTime) { return Seconds(i, otherTime); }
+		public static TimeSpan Millisecond(this double i, TimeSpan otherTime) { return Milliseconds(i, otherTime); }
+		public static TimeSpan Tick(this long i, TimeSpan otherTime) { return Ticks(i, otherTime); }
+	}
+}
