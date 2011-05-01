827b384eafdd03135c0d19abe4f6c5f3b177ddb4
John D. Hume <duelin.markers@gmail.com>
2011-05-01 07:31:09 -0400

    Add DateTimeCreation for 1.May(2011) and similar.

---
+++ FluentTime.Tests/DateTimeCreationTest.cs
@@ -0,0 +1,34 @@
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace FluentTime.Tests
+{
+	[TestFixture]
+	public class DateTimeCreationTest
+	{
+		[Test]
+		public void Creates_dates_readably ()
+		{
+			Assert.That(1.January(2001),    Is.EqualTo(new DateTime(2001, 1, 1)));
+			Assert.That(11.February(2011),  Is.EqualTo(new DateTime(2011, 2, 11)));
+			Assert.That(15.March(2011),     Is.EqualTo(new DateTime(2011, 3, 15)));
+			Assert.That(6.April(2011),      Is.EqualTo(new DateTime(2011, 4, 6)));
+			Assert.That(8.May(2011),        Is.EqualTo(new DateTime(2011, 5, 8)));
+			Assert.That(14.June(2011),      Is.EqualTo(new DateTime(2011, 6, 14)));
+			Assert.That(4.July(2011),       Is.EqualTo(new DateTime(2011, 7, 4)));
+			Assert.That(21.August(2011),    Is.EqualTo(new DateTime(2011, 8, 21)));
+			Assert.That(22.September(2011), Is.EqualTo(new DateTime(2011, 9, 22)));
+			Assert.That(31.October(2011),   Is.EqualTo(new DateTime(2011, 10, 31)));
+			Assert.That(10.November(2011),  Is.EqualTo(new DateTime(2011, 11, 10)));
+			Assert.That(25.December(2011),  Is.EqualTo(new DateTime(2011, 12, 25)));
+		}
+		
+		[Test]
+		public void Created_dates_have_Kind_Unspecified ()
+		{
+			Assert.That(5.January(2011).Kind, Is.EqualTo(DateTimeKind.Unspecified));
+		}
+	}
+}
+
---
+++ FluentTime/DateTimeCreation.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace FluentTime
+{
+	public static class DateTimeCreation
+	{
+		public static DateTime January  (this int day, int year) { return new DateTime(year, 1, day); }
+		public static DateTime February (this int day, int year) { return new DateTime(year, 2, day); }
+		public static DateTime March    (this int day, int year) { return new DateTime(year, 3, day); }
+		public static DateTime April    (this int day, int year) { return new DateTime(year, 4, day); }
+		public static DateTime May      (this int day, int year) { return new DateTime(year, 5, day); }
+		public static DateTime June     (this int day, int year) { return new DateTime(year, 6, day); }
+		public static DateTime July     (this int day, int year) { return new DateTime(year, 7, day); }
+		public static DateTime August   (this int day, int year) { return new DateTime(year, 8, day); }
+		public static DateTime September(this int day, int year) { return new DateTime(year, 9, day); }
+		public static DateTime October  (this int day, int year) { return new DateTime(year, 10, day); }
+		public static DateTime November (this int day, int year) { return new DateTime(year, 11, day); }
+		public static DateTime December (this int day, int year) { return new DateTime(year, 12, day); }
+	}
+}
